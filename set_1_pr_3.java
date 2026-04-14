7b64b2806c332c083bd4cfdd00d57a183f06a37d
khushalighodadara-spec <khushalighodadara@gmail.com>
2026-04-14 11:17:34 +0530

    Create set_1_pr_3.java

---
+++ set_1_pr_3.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class oop5{
+  public static void main(String[] args){
+    if(args.length != 2)
+    {
+      System.out.println("Enter 2 number");
+      ruturn;
+    }
+    int a = integer.parseInt(args[0]);
+    int b = integer.parseInt(args[1]);
+    int s = a+b;
+    System.out.println("sum:"+s);
+  }
+}
+  
